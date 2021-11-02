7b3a8e4ad87d3c73574785493414db9ecbc2dddd
Alice Chen <zixic@student.unimelb.edu.au>
2021-11-02 13:11:54 +1100

    remove gitignore files

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(port,console.log(`Server is listening onport ${port}...`))
+        app.listen(port,console.log(`Server is listening on port ${port}...`))
     } catch (error) {
         console.log(error)
     }
